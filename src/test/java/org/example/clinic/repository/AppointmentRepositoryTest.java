7b50cbad1a0468a8a54e2e40b1326f5d59022ce8
Grzegorz <grzegorz30017@gmail.com>
2025-06-13 23:53:23 +0200

    Configure test profile with H2 database, update dependencies, and annotate repository tests with @ActiveProfiles

--- src/test/java/org/example/clinic/repository/AppointmentRepositoryTest.java
+++ src/test/java/org/example/clinic/repository/AppointmentRepositoryTest.java
@@ -7,6 +7,7 @@ import org.example.clinic.model.User.Role;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class AppointmentRepositoryTest {
 
     @Autowired
--- src/test/java/org/example/clinic/repository/UserRepositoryTest.java
+++ src/test/java/org/example/clinic/repository/UserRepositoryTest.java
@@ -5,12 +5,14 @@ import org.example.clinic.model.User.Role;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
