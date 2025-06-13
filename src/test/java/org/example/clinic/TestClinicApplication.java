dbf8b8436ca067f59e9059d590433adfa9eb8c6f
Grzegorz <grzegorz30017@gmail.com>
2025-06-13 22:13:30 +0200

    Initial commit

---
+++ src/test/java/org/example/clinic/TestClinicApplication.java
@@ -0,0 +1,11 @@
+package org.example.clinic;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(ClinicApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
